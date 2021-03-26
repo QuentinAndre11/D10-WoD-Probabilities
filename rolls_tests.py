4a9967ff9d943e444898cd082c9ef1a13ad60d58
Quentin André <quentin.andre@imt-atlantique.net>
2021-03-26 15:24:17 +0100

    Add of tests for each function testable

---
+++ rolls_tests.py
@@ -0,0 +1,51 @@
+from rolls import *
+
+# I can't imagine a unit test for roll (because of alea)
+
+def test_initialList():
+    """Check that `initialList()` is ordered in the right order"""
+
+    L=initialList(10)
+    assert L[0] >= L[1]
+
+def test_transformedList():
+    """Check that `transformedList()` works as expected"""
+
+    L1=transformedList([10,10,6,5,1])
+    L2=transformedList([10,6,5,1,1])
+    L3=transformedList([8,6,5,1,1])
+    L4=transformedList([10,10,6,5,2])
+    assert L1 == [10,6,5]
+    assert L2 == [6,5,1]
+    assert L3 == [8,6,5,1,1]
+    assert L4 == [10,10,6,5,2]
+
+def test_countSuccess() :
+    """Check that `countSuccess()` works as expected"""
+
+    L1=[10,9,8,7,6,5,4,3,2]
+    L2=[10,10,8,7,6,5,4,3,2]
+    L3=[9,9,8,7,6,5,4,3,2]
+    L4=[9,8,7,6,5,4,3,2,1]
+    L4=[9,8,7,6,5,4,3,1,1]
+    assert countSuccess(L1,6,False) == 5
+    assert countSuccess(L1,8,False) == 3
+    assert countSuccess(L1,6,True) == 6
+    assert countSuccess(L2,6,False) == 5
+    assert countSuccess(L2,6,True) == 7
+    assert countSuccess(L3,6,False) == 5
+    assert countSuccess(L3,6,True) == 5
+    assert countSuccess(L4,6,False) == 3
+    assert countSuccess(L4,6,True) == 2
+
+def test_rollsStats() :
+    """Check that `rollsStats()` works as expected"""
+    assert rollsStats(1,6,hero=False) == [0.4, 0.5, 0.4, 0.1]
+    assert rollsStats(1,8,hero=False) == [0.2, 0.7, 0.2, 0.1]
+    assert rollsStats(1,6,hero=True) == [0.5, 0.5, 0.4, 0.1]
+    assert rollsStats(2,6,hero=False) == [0.84, 0.08, 0.28, 0.64] #verified by enumerating
+    assert rollsStats(2,6,hero=True) == [0.97, 0.08, 0.28, 0.64] #verified by enumerating
+    assert rollsStats(2,8,hero=False) == [0.51, 0.12, 0.45, 0.43] #verified by enumerating
+    assert rollsStats(2,8,hero=True) == [0.62, 0.12, 0.45, 0.43] #verified by enumerating
+
+# array and pretty array are only visuals
